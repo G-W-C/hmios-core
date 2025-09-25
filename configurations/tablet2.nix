c3829f061bf992d3517687e4658e3fb20b54047e
G-W-C <grant@gc-is.com>
2025-09-25 11:16:40 -0700

    contine working on kb

--- configurations/tablet2.nix
+++ configurations/tablet2.nix
@@ -170,15 +170,22 @@
 
     # Status Bar (minimal)
     bar {
-        position bottom
-        status_command while date +'%Y-%m-%d %I:%M:%S %p'; do sleep 1; done
-        
-        colors {
-            statusline #ffffff
-            background #323232
-            inactive_workspace #32323200 #32323200 #5c5c5c
-        }
-        height 30
+       position bottom
+       status_command while date +'%Y-%m-%d %I:%M:%S %p'; do sleep 1; done
+    
+       colors {
+           statusline #ffffff
+           background #323232
+           inactive_workspace #32323200 #32323200 #5c5c5c
+       }
+       height 30
+
+       # Custom module for keyboard toggle
+       tray_output primary
+       binding {
+           label "⌨"
+           command "/home/kiosk/bin/toggle-keyboard.sh"
+       }
     }
     exec_always /run/current-system/sw/bin/wvkbd-mobintl &
     # Auto-start applications
